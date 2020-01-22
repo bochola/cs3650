a68c685869f14d1ac9441cd16149f3dd03c19e55
Benjamin Ochola <bochola@login-students.ccs.neu.edu>
2020-01-22 02:34:54 -0500

    Finished 90% of fib.S, added ccalc executable

--- hw02/hw02-starter/calc.c
+++ hw02/hw02-starter/calc.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main(int argc, char* argv[])
+long compute(long x, long y, char op); 
+
+int main(int argc, char* argv[])
 {
     if (argc != 4) {
         printf("Usage:\n %s N op N\n", argv[0]);
         return 0;
     }
     
-    printf("Operator length?: % "
-    char op = "h";// *argv[2];
+    char op = argv[2][0]; // *argv[2];
+    printf("Operator number: %ld\n", op);
     // argv[2] might be a string, or a pointer to a string
     // if it is a pointer, idk what to do tbh
-    if ((op != "+") ||(op != "-") ||(op != "/") ||(op != "*")) {
+    
+    if (!((op == '+') || (op == '-') || (op == '/') || (op == '*'))) {
         printf("Usage:\n %s N op N, where op must be one of: +, -, *, /\n", argv[0]);
         return 0;
     }
-
+    
     long x = atol(argv[1]);
     long y = atol(argv[3]);
     
-    int ans = compute(x, y, op);
+    long ans = compute(x, y, op);
 
-    printf("%ld %ld %ld = %ld\n", x, op, y, ans);
+    printf("%ld %c %ld = %ld\n", x, op, y, ans);
     return 0;
+    
 }
 
-int compute(int x, int y, char op) {
+long compute(long x, long y, char op) {
     
     long ans;
-
-    switch(op) {
-        
-        case op == "+":
-            ans = x + y;
-            break;
-        case op == "-":
-            ans = x - y;
-            break;
-        case op == "/":
-            ans = x / y;
-            break;
-        case op == "*":
-            ans = x * y;
-            break;
+    
+    if (op == '+') {
+	ans = x + y;
     }
-
-    return ans;           
+    
+    if (op == '-') {
+        ans = x - y;
+    }
+    
+    if (op == '*') {
+        ans = x * y;
+    }
+    
+    if (op == '/') {
+        ans = x / y;
+    }
+    
+    return ans;
+           
 }
