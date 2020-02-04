f066f34579142397b7bca0c9be70eeee579be05e
Benjamin Ochola <bochola@login-students.ccs.neu.edu>
2020-02-03 22:01:32 -0500

    Fleshed out svec.c

--- hw04/starter-hw04/svec.c
+++ hw04/starter-hw04/svec.c
@@ -8,52 +8,81 @@
 
 #include "svec.h"
 
-svec*
-make_svec()
+svec* make_svec()
 {
-    svec* sv = malloc(sizeof(svec));
-    sv->data = malloc(2 * sizeof(char*));
-    sv->size = 0;
-    // TODO: correctly allocate and initialize data structure
+    svec* sv = malloc(sizeof(svec));      // sv is a pointer to an svec
+    sv->data = malloc(2 * sizeof(char*)); // sv->data is a pointer to 2 slots of char* (strings)
+    sv->size = 2;
+    // TO-DONE(?): correctly allocate and initialize data structure
+    prinf("Created svec sucessfully");
     return sv;
 }
 
-void
-free_svec(svec* sv)
+void free_svec(svec* sv)
 {
-    // TODO: free all allocated data
+    // TO-DONE: free all allocated data
+    // Will need to free each item inside sv->data, then free sv
+    
+    printf("Freeing svec data...");
+    
+    for (int ii = 0; ii < sv->size; ii++)
+    {
+        free(sv->data[ii]);
+    }
+    
+    printf("Freeing svec...");    
+    free(sv);
+    printf("Freed");
+    
 }
 
-char*
-svec_get(svec* sv, int ii)
+char* svec_get(svec* sv, int ii)
 {
-    assert(ii >= 0 && ii < sv->size);
+    assert((ii >= 0) && (ii < sv->size));
+    printf("Retrived data at location  %ld", ii);
     return sv->data[ii];
 }
 
-void
-svec_put(svec* sv, int ii, char* item)
+void svec_put(svec* sv, int ii, char* item)
 {
     assert(ii >= 0 && ii < sv->size);
-    sv->data[0] = item;
-    // TODO: insert item into slot ii
+    // Use strdup here on item
+    char* copy = strdup(item);       // since strdup uses malloc, you need to free
+    printf("Placing data at location  %ld", ii);
+    sv->data[ii] = copy;             // after you call it. i think
+    // TO-DONE: insert the given item into svec
     // Consider ownership of string in collection.
 }
 
-void
-svec_push_back(svec* sv, char* item)
+void svec_push_back(svec* sv, char* item)
 {
     int ii = sv->size;
 
-    // TODO: expand vector if backing erray
-    // is not big enough
-
-    sv->size = ii + 1;
-    svec_put(sv, ii, item);
+    // TODO: expand vector if backing array is not big enough
+    
+    // if(last-is-empty?){
+    //     svec_put(sv, ii - 1, item);
+    // }
+    // else{
+    //     int newSize = sv->size * 2;
+    //     char** temp = realloc(sv->data, newSize);
+    //     
+    //     // wait so what do i do here? how do i change sv->data to be temp? is it just
+    //     sv->data = temp;
+    //     svec_put(sv, ii,  item);
+    // }
+    
+    
 }
 
-void
-svec_swap(svec* sv, int ii, int jj)
+void svec_swap(svec* sv, int ii, int jj)
 {
-    // TODO: Swap the items in slots ii and jj
+    // TO-DONE: Swap the items in slots ii and jj
+    
+    char* slot1 = svec_get(sv, ii);
+    char* slot2 = svec_get(sv, jj);
+    
+    svec_put(sv, jj, slot1);
+    svec_put(sv, ii, slot2);
+    
 }
