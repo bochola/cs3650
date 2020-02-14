74fc997d54b779e5ca573f4e84557ff206c88906
Ben <ochola.b@husky.neu.edu>
2020-02-14 15:39:50 -0500

    Added make_op() option for astree.c

--- chal1/challenge01/astree.c
+++ chal1/challenge01/astree.c
@@ -26,7 +26,30 @@ astree* make_cmd(char* cmd) {
 
 astree* make_op(char* op, astree* arg1, astree* arg2) {
     
+    astree* ast = malloc(sizeof(astree));
+    ast->op = op;
+    ast->arg1 = 0;
+    ast->arg2 = 0;
+
+    if (cmd) {
+        ast->cmd = cmd;
+    }
+    else {
+        //Maybe exit(0); or something?
+    }
     
+    if (arg1) {
+        ast->arg1 = arg1;
+    }
+    else {
+        //Maybe exit(0); or something?
+    }
     
+    if (arg2) {
+        ast->arg2 = arg2;
+    }
+    else {
+        //Maybe exit(0); or something?
+    }
     
 }
