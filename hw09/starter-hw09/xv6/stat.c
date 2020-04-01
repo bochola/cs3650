5bf6d94cc129418f5e1dba6fc49fe683c5bcec5b
Ben <ochola.b@husky.neu.edu>
2020-03-31 23:51:07 -0400

    Started syscall getiostats

---
+++ hw09/starter-hw09/xv6/stat.c
@@ -0,0 +1,15 @@
+// My addition to this xv6 program
+
+#include "stat.h"
+/* 
+ * struct iostats {
+ *     uint read_bytes;
+ *     uint write_bytes;
+ * };
+ */
+
+// Function to return the status of the counters
+int getiostats (int fd, struct iostats* stats) {
+    // How to print while within the xv6 session? Should be a function
+
+}
